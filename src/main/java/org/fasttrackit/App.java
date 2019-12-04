a03117cf4775e070dd86086763ad2727d58e0813
BogdanBoboaca <bogdan.m.boboaca@gmail.com>
2019-12-04 21:27:39 +0200

    added example for comparing objects with equals

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -11,6 +11,14 @@ public class App
         Game game = new Game();
         game.start();
 
+        //String pull
+//        String a = "test";
+//        String b = "test";
+//        String c = new String("test");
+//
+//        System.out.println(a == b );
+//        System.out.println(a == c );
+//        System.out.println(a.equals(c));
 
         // Method implementation taken from object's class,
         // not from the variable's class
